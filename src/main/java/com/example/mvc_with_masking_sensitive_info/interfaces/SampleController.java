79822c5ed86967179a030a73e62eca2864dd29e1
kogayushi <youshe.old.pleasure@gmail.com>
2017-10-29 16:14:25 +0900

    update

--- src/main/java/com/example/mvc_with_masking_sensitive_info/interfaces/SampleController.java
+++ src/main/java/com/example/mvc_with_masking_sensitive_info/interfaces/SampleController.java
@@ -5,15 +5,19 @@ import com.example.mvc_with_masking_sensitive_info.interfaces.dto.SampleResponse
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@SessionAttributes(types = {})
 public class SampleController {
 
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    @PostMapping("/form")
+    @PostMapping(value = "/form", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public SampleResponseDto sample(@RequestParam String userId, @RequestParam String password) {
         log.info("I'm in form");
         return new SampleResponseDto(userId, password);
@@ -23,10 +27,26 @@ public class SampleController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @PostMapping(value = "/json", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public SampleResponseDto sample(@RequestBody SampleRequestDto payload) {
+    public SampleResponseDto sample(@Validated @RequestBody SampleRequestDto payload) {
         log.info("I'm in json");
         SampleResponseDto response = new SampleResponseDto(payload.getUserId(), payload.getPassword());
         return response;
 
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handle(MissingServletRequestParameterException ex) {
+        log.error("exception", ex);
+        return ex.getMessage();
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handle2(MethodArgumentNotValidException ex) {
+        log.error("exception", ex);
+        return ex.getMessage();
+    }
+    //MethodArgumentNotValidException
+
+    @RequestMapping(params = "_step=1")
 }
--- src/main/java/com/example/mvc_with_masking_sensitive_info/interfaces/dto/SampleRequestDto.java
+++ src/main/java/com/example/mvc_with_masking_sensitive_info/interfaces/dto/SampleRequestDto.java
@@ -2,10 +2,13 @@ package com.example.mvc_with_masking_sensitive_info.interfaces.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
 @NoArgsConstructor
 public class SampleRequestDto {
+    @NotEmpty
     private String userId;
+    @NotEmpty
     private String password;
 }
